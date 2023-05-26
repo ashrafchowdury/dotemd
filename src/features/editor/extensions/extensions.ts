450878fe9117ffdb37f5a1ecb0b1d12958e994c1
Ashraf <ashrafdev365@gmail.com>
2023-05-25 22:25:37 -1200

    feature - Added trackable placeholder text to track the cursor and show the text according to the format

--- src/features/editor/extensions/extensions.ts
+++ src/features/editor/extensions/extensions.ts
@@ -1,5 +1,6 @@
 import StarterKit from "@tiptap/starter-kit";
 import Heading from "@tiptap/extension-heading";
+import Placeholder from "@tiptap/extension-placeholder";
 
 export const extensions = [
   StarterKit.configure({
@@ -14,5 +15,24 @@ export const extensions = [
   }),
   Heading.configure({
     levels: [1, 2, 3],
+  }),
+  Placeholder.configure({
+    emptyEditorClass: "placeholder",
+    includeChildren: true,
+    placeholder: ({ node }) => {
+      const element: string | undefined = (window.getSelection() as any)
+        ?.baseNode?.offsetParent?.localName;
+      if (node.type.name == "heading") {
+        return "Heading";
+      } else if (node.type.name == "codeBlock") {
+        return "Add Code";
+      } else if (element == "th") {
+        return "Table heading";
+      } else if (element == "td") {
+        return "Table info";
+      } else {
+        return "Type '/' To Open Selector";
+      }
+    },
   }),
 ];
