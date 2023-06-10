3b6527929a284bd172a13de21f583bdc2c8bb671
Ashraf <ashrafdev365@gmail.com>
2023-06-10 01:17:01 -1200

    feature - Added error, loading, and not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className=" h-[80vh] flex flex-col justify-center items-center">
+      <p className=" lg:text-4xl md:text-3xl text-2xl font-semibold mb-12 text-center">
+        Page Not Found 👎
+      </p>
+    </div>
+  );
+}
